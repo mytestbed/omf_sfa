a50ce608200783bc0521baa76c7f1cb1662ee44a
Aris Dadoukis <ardadouk@gmail.com>
2013-06-11 21:35:12 +0300

    change lease status to accepted and some minor changes

--- lib/omf-sfa/am/am_scheduler.rb
+++ lib/omf-sfa/am/am_scheduler.rb
@@ -18,7 +18,7 @@ module OMF::SFA::AM
     # Create a resource of specific type given its description in a hash. If the type
     # or the resource is physical then we create a clone of itself and assign it to
     # the user who asked for it (conceptually a physical resource even though it is exclusive,
-    # is never given to the user but instead we provide him a clone of the resource). 
+    # is never given to the user but instead we provide him a clone of the resource).
     # If the type is a 'lease' then we normally create a lease object.
     #
     # @param [Hash] resource_descr contains the properties of the new resource
@@ -57,6 +57,7 @@ module OMF::SFA::AM
         lease = OMF::SFA::Resource::OLease.create(resource_descr)
         lease.valid_from = oproperties[:valid_from]
         lease.valid_until = oproperties[:valid_until]
+        #lease.status = :pending
         raise UnavailableResourceException.new "Cannot create '#{resource_descr.inspect}'" unless lease.save
         return lease
       else
@@ -75,21 +76,20 @@ module OMF::SFA::AM
       unless resource.is_a? OMF::SFA::Resource::OResource
         raise "Expected OResource but got '#{resource.inspect}'"
       end
+
       base = resource.provided_by
       base.leases.each do |l|
-	debug l.to_json
-	if (l.id == resource.leases.first.id)
-	  time = Time.now
-	  if (l.valid_until < time)
-	    l.status = "past"
-	  else
-	    l.status = "cancelled"
-	  end
-	end
+        if (l.id == resource.leases.first.id)
+          time = Time.now
+          if (l.valid_until <= time)
+            l.status = "past"
+          else
+            l.status = "cancelled"
+          end
+        end
       end
       msg = resource.leases.first.component_leases.destroy!
       raise "Failed to destroy component_leases" unless msg
-      
       resource = resource.destroy!
       raise "Failed to destroy resource" unless resource
       resource
@@ -104,25 +104,25 @@ module OMF::SFA::AM
     def lease_component(lease, component)
       # TODO: Implement a basic FCFS for the competing leases of a component.
       # Basic Component provides itself(clones) so many times as the accepted leases on it.
-      #debug "lease_component: lease:'#{lease.name}' to component:'#{component.name}'"
+      debug "lease_component: lease:'#{lease.name}' to component:'#{component.name}'"
 
       base = component.provided_by
       base.leases.each do |l|
-	if (lease.valid_from >= l.valid_until || lease.valid_until <= l.valid_from)
-	  #all ok, do nothing
-	elsif (lease.valid_from <= l.valid_from && lease.valid_until > l.valid_from)#overlapping time
-	  raise UnavailableResourceException.new "Cannot lease '#{component.name}', because it is unavailable for the requested time."
-	elsif (lease.valid_from >= l.valid_from && lease.valid_from <= l.valid_until)#overlapping time
-	  raise UnavailableResourceException.new "Cannot lease '#{component.name}', because it is unavailable for the requested time."
-	end
+        if (lease.valid_from >= l.valid_until || lease.valid_until <= l.valid_from)
+          #all ok, do nothing
+        elsif (lease.valid_from <= l.valid_from && lease.valid_until > l.valid_from)#overlapping time
+          raise UnavailableResourceException.new "Cannot lease '#{component.name}', because it is unavailable for the requested time."
+        elsif (lease.valid_from >= l.valid_from && lease.valid_from <= l.valid_until)#overlapping time
+          raise UnavailableResourceException.new "Cannot lease '#{component.name}', because it is unavailable for the requested time."
+        end
       end
-       
+      lease.status = "accepted"
       base.leases << lease
       base.save
       component.leases << lease
       component.save
       #@am_liaison.enable_lease(lease, component)
-      return component #for testing purposes. should return true
+      return true
     end
 
     # It returns the default account, normally used for admin account.
@@ -130,13 +130,13 @@ module OMF::SFA::AM
     # @return [OAccount] returns the default account object
     #
     def get_nil_account()
-      @nil_account
+        @nil_account
     end
 
     def initialize()
       @nil_account = OMF::SFA::Resource::OAccount.new(:name => '__default__', :valid_until => Time.now + 1E10)
       @am_liaison = OMF::SFA::AM::AMLiaison.new
-    end    
+    end
 
   end # OMFManager
 
